91ae687840262b0578592435b5d650dff8899631
Mikhaylov-yv <dezmond22@yandex.ru>
2021-02-08 19:31:06 +0300

    Добавил коментарии

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import pickle
 import pandas as pd
 
 def main(df):
+    # Load model
     tfidf = pickle.load(open('tfidf', 'rb'))
     clf = pickle.load(open('clf_task1', 'rb'))
-
+    # Edit data
     X_test = tfidf.transform(df.item_name)
-
+    # Predict
     pred = clf.predict(X_test)
-
+    # generation report
     res = pd.DataFrame(pred, columns=['pred'])
     res['id'] = df['id']
 
--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from main import main
 
-
+# Load_data
 test = pd.read_parquet('data/task1_test_for_user.parquet')
+# Predict
 main(test)
